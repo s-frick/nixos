ffb3c78e4faf2ab209e2eccb2628370f2a6c5b8c
Sebastian Frick <sebastian.frick@mail.de>
2026-04-14 16:25:05 +0200

    wsl support

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -12,10 +12,18 @@
   environment.systemPackages = with pkgs; [
     git-credential-oauth
   ];
+  # vscode wsl nixos support
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    openssl
+    curl
+    zlib
+    icu
+  ];
 
   # wsl-spezifische Home-Manager-Erweiterungen für sebi
   home-manager.users.sebi.imports = [
     ./home.nix
   ];
 }
-
